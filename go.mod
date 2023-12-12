89d80be29855ef41ac6f578ff63df0e609d4e86d
juhayahoo <juhagithub@gmail.com>
2023-12-12 22:46:10 +1100

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/juhayahoo/puppy
 
 go 1.21.4
+
+require github.com/juhayahoo/dog v0.0.0-20231212112426-8ac8dbe23a79 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/juhayahoo/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
-	return "Woof Woof!!"
+	return "Woof Woo f!!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Barks())
 }
